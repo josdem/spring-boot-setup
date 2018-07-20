a135dc23f115704d7d8d124e1f6282c0b5e2d266
josdem <joseluis.delacruz@gmail.com>
2018-07-20 16:58:57 -0400

    Updating packages structure

--- src/main/java/com/jos/dem/springboot/setup/DemoApplication.java
+++ src/main/java/com/jos/dem/springboot/setup/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.example.springbootsetup;
+package com.jos.dem.springboot.setup;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,5 +8,6 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-	}
+  }
+
 }
