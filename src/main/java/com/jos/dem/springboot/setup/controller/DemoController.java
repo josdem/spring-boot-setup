53e930932c29a3f123824bc78e359007d66dda5f
josdem <joseluis.delacruz@gmail.com>
2020-08-21 13:05:48 -0400

    Updating files

--- src/main/java/com/jos/dem/springboot/setup/controller/DemoController.java
+++ src/main/java/com/jos/dem/springboot/setup/controller/DemoController.java
@@ -1,15 +1,13 @@
-package com.jos.dem.springboot.setup;
+package com.jos.dem.springboot.setup.controller;
 
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
 
   @RequestMapping("/")
-  public String index(){
+  public String index() {
     return "Hello World!";
   }
-
 }
-
